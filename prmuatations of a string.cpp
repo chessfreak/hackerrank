7dd0fd594bbe18feee98cd3de125aeee37feaaec
unknown <chessfreak>
2021-01-02 18:50:02 +0530

    permutations of a string

---
+++ prmuatations of a string.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(char *a, char *b)
+{
+    char temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+
+}
+
+
+void permute(char *a, int l, int r)
+{
+	int i;
+	if (l == r)
+		cout<<a<<endl;
+	else
+	{
+		for (i = l; i <= r; i++)
+		{
+			swap((a+l), (a+i));
+			permute(a, l+1, r);
+			swap((a+l), (a+i)); //backtrack
+		}
+	}
+}
+int main()
+{
+    char str[]="abcd";
+    int n=strlen(str);
+    permute(str,0,n-1);
+    return 0;
+
+}
